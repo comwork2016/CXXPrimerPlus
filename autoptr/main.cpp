4ee8c52a917a625937b1d5c22bf3365820b1d4d7
comwork2016 <comwork2016@sina.com>
2016-09-23 14:12:41 +0800

    autoptr

---
+++ autoptr/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <memory>
+
+using namespace std;
+
+class Test
+{
+public:
+    Test(string s)
+    {
+        str = s;
+        cout<<"String "<<s<<" create\n";
+    }
+
+    ~Test()
+    {
+        cout<<"Test delete:"<<str<<endl;
+    }
+
+    string& getStr()
+    {
+        return str;
+    }
+
+    void setStr(string s)
+    {
+        str = s;
+    }
+
+    void print()
+    {
+        cout<<str<<endl;
+    }
+private:
+    string str;
+};
+
+
+int main()
+{
+    auto_ptr<Test> ptest(new Test("123"));
+    ptest->setStr("hello ");
+    ptest->print();
+    ptest.get()->print();
+    ptest->getStr() += "world !";
+    (*ptest).print();
+    ptest.reset(new Test("123"));
+    ptest->print();
+    return 0;
+}
